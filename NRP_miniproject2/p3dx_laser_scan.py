e934c8ba97016183335555ad048e77c279fd4003
lguyot <luc.guyot@epfl.ch>
2019-02-14 17:37:59 +0100

    Removes renamed files and folders

--- NRP_miniproject2/p3dx_laser_scan.py
+++
@@ -1,6 +0,0 @@
-@nrp.MapRobotSubscriber('scan', Topic('/p3dx/laser/scan', sensor_msgs.msg.LaserScan))
-@nrp.Robot2Neuron()
-def p3dx_laser_scan(t, scan):
-    # Auto generated TF for p3dx_laser_scan
-    if t % 2 < 0.02:
-        clientLogger.info('TF p3dx_laser_scan:', t)
